1de216219675c72d5e6fdd98fcb20d15556bd284
Rubens Lyra <rubensvlyra@gmail.com>
2023-12-29 15:30:10 -0300

    base adicionada

---
+++ index.js
@@ -0,0 +1,64 @@
+// index.js
+
+const tableBody = document.getElementById('tableBody');
+const pagination = document.getElementById('pagination');
+const perPage = 6; // Número de itens por página
+let currentPage = 1;
+
+function fetchData(page) {
+    const url = `https://reqres.in/api/users?page=${page}&per_page=${perPage}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            // Limpa a tabela e adiciona os novos dados
+            tableBody.innerHTML = '';
+            data.data.forEach(user => {
+                const tableBodyRow = document.createElement('tr');
+                const tableBodyRowDataName = document.createElement('td');
+                tableBodyRowDataName.textContent = user.first_name;
+                const tableBodyRowDataSurname = document.createElement('td');
+                tableBodyRowDataSurname.textContent = user.last_name;
+                const tableBodyRowDataEmail = document.createElement('td');
+                tableBodyRowDataEmail.textContent = user.email;
+                const tableBodyRowDataAvatar = document.createElement('td');
+                const avatarImg = document.createElement('img');
+                avatarImg.src = user.avatar;
+                avatarImg.alt = 'Avatar';
+                tableBodyRowDataAvatar.appendChild(avatarImg);
+
+                tableBodyRow.appendChild(tableBodyRowDataName);
+                tableBodyRow.appendChild(tableBodyRowDataSurname);
+                tableBodyRow.appendChild(tableBodyRowDataEmail);
+                tableBodyRow.appendChild(tableBodyRowDataAvatar);
+                tableBody.appendChild(tableBodyRow);
+            });
+
+            // Adiciona a paginação
+            addPagination(data.total_pages);
+        })
+        .catch(error => console.error(error));
+}
+
+function addPagination(totalPages) {
+    pagination.innerHTML = '';
+
+    for (let i = 1; i <= totalPages; i++) {
+        const li = document.createElement('li');
+        li.classList.add('page-item');
+        const a = document.createElement('a');
+        a.classList.add('page-link');
+        a.href = '#';
+        a.textContent = i;
+        a.addEventListener('click', () => {
+            currentPage = i;
+            fetchData(currentPage);
+        });
+
+        li.appendChild(a);
+        pagination.appendChild(li);
+    }
+}
+
+// Inicializa com a primeira página
+fetchData(currentPage);
